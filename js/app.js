4f838e1b916e99c24e34ec0fd70e99461b6fb19a
daniel <daniel@gmail.com>
2026-09-25 07:11:50 -0500

    Login terminado

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-.then(response => response.json())
-.then(data => { console.log(data); console.log(data[0]); console.log(data[0].name);
-})
-.catch(error => console.error(error));
+// Define la URL de la API para un solo elemento todo
+const apiUrl = "https://fakestoreapi.com/docs";
+
+// Realiza una solicitud GET utilizando la API fetch
+fetch(apiUrl)
+  .then((response) => {
+    // La función fetch devuelve una promesa.
+    // El primer bloque .then() recibe el objeto Response.
+    // Necesitamos llamar al método .json() en la respuesta para analizar el cuerpo del texto como JSON.
+    return response.json();
+  })
+  .then((data) => {
+    // El segundo bloque .then() recibe los datos JSON analizados.
+    console.log("Datos obtenidos exitosamente:");
+    console.log(data);
+  })
+  .catch((error) => {
+    // El bloque .catch() se ejecutará si ocurre algún error durante la operación fetch.
+    console.error("Error al obtener datos:", error);
+  });
 
 document.addEventListener("DOMContentLoaded", function() {
     var loginForm = document.getElementById("loginForm");
@@ -12,8 +27,8 @@ document.addEventListener("DOMContentLoaded", function() {
             var username = document.getElementById("username").value;
             var password = document.getElementById("password").value;
 
-            
-            if (username === "admin" && password === "1234") {
+            //VALIDAR USUARIO
+            if (username === "mor_2314" && password === "83r5^_") {
                 window.location.href = "dashboard.html";
             } else {
                 alert("Usuario o contraseña incorrectos");
